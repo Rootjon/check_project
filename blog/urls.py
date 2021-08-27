c5288aeca5de9b9ba76506483e2404bf2cc1ca82
Rootjon <ahmadjonaid9@gmail.com>
2021-08-27 23:48:12 +0300

    updatehjkl

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name='blog'
 
 urlpatterns = [
     path('',views.blog_list,name='blog_list'),
-    path('<slug:category_slug>', views.category, name='blog_by_category'),
+    path('<slug:category_slug>/', views.category, name='blog_by_category'),
     path('search/', views.search_blog, name='search_blog'),
     path('neww/', views.new, name='new'),
 
